17cee6684a5320b93c70767bb733da8501e12171
Zankoden <zankodits@gmail.com>
2024-07-06 20:00:43 +0545

    new data

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ function App() {
   return (
     <>
       <Student name="Itachi Uchiha" age ={30} isStudent ={true}/>
+      <Student name="Edward Newgate" age ={72} isStudent ={true}/>
+      <Student name="Whitebeard" age ={72} isStudent ={true}/>
     </>
   );
 }
