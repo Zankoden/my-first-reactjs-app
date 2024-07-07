2a78bdf0a3f2e8afa8a252820bdee7e98a9c32c8
Zankoden <zankodits@gmail.com>
2024-07-07 16:51:19 +0545

    added more data

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,9 @@ function App() {
   return (
     <>
       <Student name="Itachi Uchiha" age ={30} isStudent ={true}/>
-      <Student name="Edward Newgate" age ={72} isStudent ={true}/>
-      <Student name="Whitebeard" age ={72} isStudent ={true}/>
+      <Student name="Edward Newgate" age ={72} isStudent ={false}/>
+      <Student name="Whitebeard" age ={72} isStudent ={false}/>
+      <Student name="Portgas D. Ace" age ={23} isStudent ={false}/>
     </>
   );
 }
